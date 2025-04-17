3f443fe58539276ad85e3cf0d9a570af539f26f4
dpavon-g <pavongomezdaniel@gmail.com>
2025-04-17 12:37:02 +0200

    Deploy of react native working

---
+++ Docker_Deployers/React_Native_Deploy/docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APP_DIR="/app"
+PROJECT_NAME="my-app"
+PROJECT_PATH="$APP_DIR/$PROJECT_NAME"
+
+cd $APP_DIR
+
+if [ ! -d "$PROJECT_PATH" ]; then
+  echo "🚧 Proyecto no encontrado. Creando $PROJECT_NAME..."
+  npx --yes create-expo-app $PROJECT_NAME --template blank
+  cd $PROJECT_NAME
+  npm install
+else
+  echo "✅ Proyecto encontrado. Iniciando Expo..."
+  cd $PROJECT_NAME
+fi
+
+chown -R 1000:www-data .
+
+exec npx expo start --lan --clear
+
